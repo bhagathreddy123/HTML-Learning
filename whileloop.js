9aa74b617f224c22ec9835325c14e78b071ffa2e
bhagath <bhagath@getcustomexposure.com>
2018-02-09 17:12:24 +0530

    while loop structure in javaScript

---
+++ whileloop.js
@@ -0,0 +1,33 @@
+// var i = -10;
+
+// while(i < 10) {
+// 	console.log(i);
+// 	i++;
+
+// }
+	
+// 	var i = 10;
+
+// while(i <= 40) {
+// 	console.log(i);
+// 	i+=2;
+// }
+	
+// 		var i = 300;
+
+// while(i <= 333) {
+// 	if(i%2 !== 0) {
+// 		console.log(i);
+// 	}
+// 	i+=1;
+// }
+// 	
+
+		var i = 5;
+
+while(i <= 50) {
+	if(i%5 == 0 && i%3 == 0) {
+		console.log(i);
+	}
+	i+=1;
+}
