598957dab85179c3b838874c669e395217f7be7a
bhagath <bhagath@getcustomexposure.com>
2018-02-09 11:08:41 +0530

    first name and lastname

---
+++ script.js
@@ -0,0 +1,15 @@
+/*var userName = window.prompt('enter your Name');*/
+/*var userName = prompt('enter your Name');*/
+
+/*alert(userName);*/
+/*alert("Nice to see you."  + userName);
+console.log("Nice to see You" + userName);*/
+
+var firstName = prompt("enter your First Name");
+var lastName = prompt("enter your Last Name");
+var userAge = prompt("enter your Age");
+
+
+
+console.log("Your Full Name is " + firstName  + "" + lastName); 
+console.log("your age is" +userAge);
