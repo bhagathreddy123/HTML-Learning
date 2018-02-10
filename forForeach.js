2258eb1b3bfd031bd68d8066eedce9be3eb23e96
bhagath <bhagath@getcustomexposure.com>
2018-02-10 22:21:40 +0530

    for foreach for adding listing todos

---
+++ forForeach.js
@@ -0,0 +1,13 @@
+var colours = ["red","green","yellow","orange"];
+
+// console.log(colours[1]);
+
+// for(i=0; i<colours.length; i++)
+// {
+// 	console.log(colours[i]);
+// }
+
+
+colours.forEach(function(colour){
+	console.log(colour);
+});
