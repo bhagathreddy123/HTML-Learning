c0cc7a3fc18b1b66942a7bcf5b4ba97d7b251fd4
bhagath <bhagath@getcustomexposure.com>
2018-02-10 12:13:32 +0530

    return function usage

---
+++ javascriptfunc.js
@@ -0,0 +1,21 @@
+function sayHi(){
+	console.log('Hi');
+}
+
+function add(a,b)
+{
+	console.log(a+b);
+}
+
+function person(bhagath,reddy,katta)
+{
+	console.log("firstname:" + bhagath)
+	console.log("middlename:" + reddy)
+	console.log("lastname:" + katta)
+}
+person("sanjay","rahul")
+
+sayHi()
+sayHi()
+add(11,3)
+add(5,8)
---
+++ return.js
@@ -0,0 +1,28 @@
+/* function doesn't return anything inside console we will get message. print something  thins is function declaration function calling*/
+// function sayHi() {
+// 	console.log("say Hello");
+// }
+
+// sayHi()
+
+// return something.
+
+// function sayHi() {
+// 	return "say Hello"
+// }
+
+
+// function declaration
+
+// function capitalize(str) 
+// {
+// 	return str.charAt(0).toUpperCase() + str.slice(1);
+// }
+
+// function expression
+var capitalize = function(str) {
+	return str. charAt(0).toUpperCase() + str.slice(1);
+}
+
+var a = 10;
+var capitalize = a;
