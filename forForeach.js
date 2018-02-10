8bf164499752980c064a0e0e8e0b807b9136ffa7
bhagath <bhagath@getcustomexposure.com>
2018-02-10 23:08:50 +0530

    todo list and and add todo delete todo

--- forForeach.js
+++ forForeach.js
@@ -1,13 +1,22 @@
-var todos = ['preparing dinner'];
+var todos = ['swert'];
 
 var input = prompt("what would you like to do?");
 
 while(input !== "quit") {
 	if(input === "list") {
-		console.log(todos);
+		console.log("*************************************");
+		todos.forEach(function(todo, i){
+			console.log(i + ": " + todo);
+		});
+			console.log("*************************************");
 	} else if(input === "new") {
 		var newTodo = prompt("Enter new todo");
 		todos.push(newTodo);
+		console.log("new todo");
+	} else if( input === "delete"){
+		var index = prompt("Enter index of todo to delete");
+		todos.splice(index,1);
+		console.log("todo deleted");
 	}
 	input = prompt("What would you like to do?");
 }
