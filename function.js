b27cebc170f7bd87007f5edf3733acc2b72b53eb
bhagath <bhagath@getcustomexposure.com>
2018-02-10 13:12:06 +0530

    javascript function examples

---
+++ function.js
@@ -0,0 +1,50 @@
+// function evenOdd(q) {
+// 	if( q % 2 === 0 )
+// 	{
+// 		return true;
+// 	}
+// 	else {
+// 		return false;
+// 	}
+// }
+
+// refactor the code
+
+
+// function evenOdd(q) {
+// 	return q % 2 === 0 
+	
+// }
+
+//return factorial
+
+// function factorial(num)
+// {
+// 	var result = 1;
+// for( var i=2; i<=num; i++)
+// {
+// 	result *= i;
+// }
+// 	return result;
+// }
+
+
+// function factorial(num) 
+// {
+// if(num === 0)
+// {
+// 	return 1;
+// }
+// var result = num;
+// for(var i = num-1; i >= 1; i--)
+// {
+// 	result *= i;
+// }
+// 	return result;
+// }
+
+
+function kebabToSnake(str) {
+	var newStr = str.replace(/-/, "_");
+	return newStr
+}
