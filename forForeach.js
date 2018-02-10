9ad283b2ec0909a528b2c234a2aabf7e6f8c839e
bhagath <bhagath@getcustomexposure.com>
2018-02-10 23:17:27 +0530

    refactor todo list

--- forForeach.js
+++ forForeach.js
@@ -4,20 +4,34 @@ var input = prompt("what would you like to do?");
 
 while(input !== "quit") {
 	if(input === "list") {
-		console.log("*************************************");
-		todos.forEach(function(todo, i){
-			console.log(i + ": " + todo);
-		});
-			console.log("*************************************");
+		listTodos();
 	} else if(input === "new") {
-		var newTodo = prompt("Enter new todo");
-		todos.push(newTodo);
-		console.log("new todo");
+		addTodo();
 	} else if( input === "delete"){
-		var index = prompt("Enter index of todo to delete");
-		todos.splice(index,1);
-		console.log("todo deleted");
+		deleteTodo();
 	}
 	input = prompt("What would you like to do?");
 }
 console.log("Ok, You Quit the app")
+
+
+function listTodos(){
+	console.log("*************************************");
+		todos.forEach(function(todo, i){
+			console.log(i + ": " + todo);
+		});
+	console.log("*************************************");
+}
+
+
+function addTodo(){
+	var newTodo = prompt("Enter new todo");
+		todos.push(newTodo);
+		console.log("new todo");
+}
+
+function deleteTodo(){
+	var index = prompt("Enter index of todo to delete");
+		todos.splice(index,1);
+		console.log("todo deleted");
+}
